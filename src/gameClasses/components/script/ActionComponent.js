f8a8f907c8f92c3556a51710acd5d909746fb72e
Jaeyun Noh <jaeyun@gmail.com>
2022-09-11 18:20:08 -0700

    aauugh tinkering with runOnClient. I think it should be one or another (client or server)

--- src/gameClasses/components/script/ActionComponent.js
+++ src/gameClasses/components/script/ActionComponent.js
@@ -20,7 +20,7 @@ var ActionComponent = IgeEntity.extend({
 
 			// if action is disabled
 			if (!action || action.disabled == true || 
-				// (ige.isServer && !action.runOnServer) || // don't run on server-side if runOnServer is disabled
+				(ige.isServer && action.runOnClient) || // don't run on server-side if runOnClient is enabled
 				(ige.isClient && !action.runOnClient) // don't run on client-side if runOnClient is diabled
 			) {
 				continue;
