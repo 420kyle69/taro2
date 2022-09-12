5d60c914f4ec598fc7693b4c6546c8c16a1e4bea
Jaeyun Noh <jaeyun@gmail.com>
2022-09-12 14:18:18 -0700

    bugfix: playerAttribute becoming 0 or full was causing crash

--- src/gameClasses/components/unit/AttributeComponent.js
+++ src/gameClasses/components/unit/AttributeComponent.js
@@ -223,12 +223,19 @@ var AttributeComponent = IgeEntity.extend({
 								self._entity.ai.announceDeath();
 							}
 							ige.script.trigger(`${this._entity._category}AttributeBecomesZero`, triggeredBy);
-							self._entity.script.trigger(`entityAttributeBecomesZero`, triggeredBy);
+
+							// necessary as self._entity can be 'player' and it doesn't have scriptComponent
+							if (self._entity._category == 'unit' || self._entity._category == 'item' || self._entity._category == 'projectile') {
+								self._entity.script.trigger(`entityAttributeBecomesZero`, triggeredBy);	
+							}
 						} else if (newValue >= attribute.max) // when attribute becomes full, trigger attributeBecomesFull event
 						{
-							// console.log("update attr fire!")
 							ige.script.trigger(`${this._entity._category}AttributeBecomesFull`, triggeredBy);
-							self._entity.script.trigger(`entityAttributeBecomesFull`, triggeredBy);
+							
+							// necessary as self._entity can be 'player' and it doesn't have scriptComponent
+							if (self._entity._category == 'unit' || self._entity._category == 'item' || self._entity._category == 'projectile') {
+								self._entity.script.trigger(`entityAttributeBecomesFull`, triggeredBy);
+							}
 						}
 
 						// check if user breaks his highscore then assign it to new highscore
