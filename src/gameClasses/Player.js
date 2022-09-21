a1c02bd72b30b8be718c1df365f222a32d938a02
Jaeyun Noh <jaeyun@gmail.com>
2022-09-21 08:32:30 -0700

    AI players won't be removed. I also made this change in the previous commit;;

--- src/gameClasses/Player.js
+++ src/gameClasses/Player.js
@@ -343,14 +343,15 @@ var Player = IgeEntity.extend({
 	},
 
 	remove: function () {
-		if (ige.isServer) {
-			const i = ige.server.developerClientIds.indexOf(this._stats.clientId);
-			if (i != -1) ige.server.developerClientIds.splice(i, 1);
-		}
-
 		// AI players cannot be removed
 		if (this._stats.controlledBy == 'human') // do not send trigger for neutral player
 		{
+
+			if (ige.isServer) {
+				const i = ige.server.developerClientIds.indexOf(this._stats.clientId);
+				if (i != -1) ige.server.developerClientIds.splice(i, 1);
+			}
+
 			ige.script.trigger('playerLeavesGame', { playerId: this.id() });
 			// session is in second
 			ige.clusterClient && ige.clusterClient.emit('log-session-duration', (Date.now() - this._stats.jointsOn) / 1000);
