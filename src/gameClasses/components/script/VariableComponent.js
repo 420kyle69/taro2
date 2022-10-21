eabea5468b07f27bfe68c6f3304aa3e39401e14a
Jaeyun Noh <jaeyun@gmail.com>
2022-10-21 15:09:14 -0700

    added handler for position being invalid when checking for entities in raycast

--- src/gameClasses/components/script/VariableComponent.js
+++ src/gameClasses/components/script/VariableComponent.js
@@ -1215,20 +1215,22 @@ var VariableComponent = IgeEntity.extend({
 					var positionA = self.getValue(text.positionA, vars);
 					var positionB = self.getValue(text.positionB, vars);
 
-					positionA = {
-						x: positionA.x / ige.physics._scaleRatio,
-						y: positionA.y / ige.physics._scaleRatio
-					};
-					positionB = {
-						x: positionB.x / ige.physics._scaleRatio,
-						y: positionB.y / ige.physics._scaleRatio
-					};
-					ige.raycaster.raycastLine(
-						positionA,
-						positionB,
-					);
-
-					returnValue = ige.game.entitiesCollidingWithLastRaycast;
+					if (positionA && positionB) {
+						positionA = {
+							x: positionA.x / ige.physics._scaleRatio,
+							y: positionA.y / ige.physics._scaleRatio
+						};
+						positionB = {
+							x: positionB.x / ige.physics._scaleRatio,
+							y: positionB.y / ige.physics._scaleRatio
+						};
+						ige.raycaster.raycastLine(
+							positionA,
+							positionB,
+						);
+	
+						returnValue = ige.game.entitiesCollidingWithLastRaycast;
+					}					
 					break;
 				}
 				case 'entityLastRaycastCollisionPosition':
