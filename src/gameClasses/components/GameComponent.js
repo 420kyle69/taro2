92efcd40781361f873662410db0fee82bd838600
Pranshu Jain <re.pranshu@gmail.com>
2022-10-26 13:09:03 +0530

    Some Mod Fixes

--- src/gameClasses/components/GameComponent.js
+++ src/gameClasses/components/GameComponent.js
@@ -124,7 +124,7 @@ var GameComponent = IgeEntity.extend({
 			var isOwner = ige.server.owner == data._id && data.controlledBy == 'human';
 			var isInvitedUser = false;
 			if (ige.game.data.defaultData && ige.game.data.defaultData.invitedUsers) {
-				isInvitedUser = ige.game.data.defaultData.invitedUsers.some(e => e.user === data._id);
+				isInvitedUser = ige.game.data.defaultData.invitedUsers.some(e => e.user === data._id && e.role === 'contributor');
 			}
 			var isUserAdmin = false;
 			var isUserMod = false;
