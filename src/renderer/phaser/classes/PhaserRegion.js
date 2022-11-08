38d010361643c700c0528f049bc977595320d2e8
Grenagar <62058278+Grenagar@users.noreply.github.com>
2022-11-08 12:01:16 +0100

    edit region on click with cursor tool

--- src/renderer/phaser/classes/PhaserRegion.js
+++ src/renderer/phaser/classes/PhaserRegion.js
@@ -27,13 +27,13 @@ var PhaserRegion = /** @class */ (function (_super) {
         gameObject.setPosition(stats.x + stats.width / 2, stats.y + stats.height / 2);
         gameObject.setInteractive();
         gameObject.on('pointerdown', function () {
-            if (ige.developerMode.active && _this.devModeScene.regionEditor.regionTool && _this.scene.input.manager.activePointer.rightButtonDown()) {
+            if (ige.developerMode.active && _this.devModeScene.devModeTools.cursorButton.active) {
                 _this.scene.input.setTopOnly(true);
                 _this.devModeScene.regionEditor.addClickedList({ name: _this.entity._stats.id, x: stats.x, y: stats.y, width: stats.width, height: stats.height });
             }
         });
         gameObject.on('pointerup', function () {
-            if (ige.developerMode.active && _this.devModeScene.regionEditor.regionTool) {
+            if (ige.developerMode.active && _this.devModeScene.devModeTools.cursorButton.active) {
                 _this.scene.input.setTopOnly(false);
                 _this.devModeScene.regionEditor.showClickedList();
             }
