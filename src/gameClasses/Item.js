65c678503281102e058abeb8b70c84a659552416
Jaeyun Noh <jaeyun@gmail.com>
2022-11-11 14:25:02 -0800

    added option to NOT run physics on clientside... again...

--- src/gameClasses/Item.js
+++ src/gameClasses/Item.js
@@ -304,7 +304,6 @@ var Item = IgeEntityPhysics.extend({
 				if (ige.physics && self._stats.type == 'weapon') {
 					if (self._stats.isGun) {
 						if (self._stats.bulletStartPosition) {
-							console.log()
 							var rotate = this._rotate.z;
 							if (owner && self._stats.currentBody && self._stats.currentBody.jointType == 'weldJoint') {
 								rotate = owner._rotate.z;
@@ -331,7 +330,10 @@ var Item = IgeEntityPhysics.extend({
 								y: (owner._translate.y + self.anchoredOffset.y) + (self._stats.bulletStartPosition.x * Math.sin(offsetAngle)) - (bulletY * Math.cos(offsetAngle))
 							};
 
-							if (this._stats.isGun) {
+							if (
+								this._stats.isGun &&
+								(ige.isServer || (ige.isClient && ige.physics)) // render projectile on clientside if physics is enabled
+							) {
 								var defaultData = {
 									rotate: rotate,
 									translate: bulletStartPosition
