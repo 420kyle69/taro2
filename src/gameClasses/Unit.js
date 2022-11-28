883635ff1f7bc6d657e539f4ed2936814b27d622
sloont <colinabel93@gmail.com>
2022-11-28 11:50:43 -0500

    removed comments from old try at attribute stream fix in changeUnitType

--- src/gameClasses/Unit.js
+++ src/gameClasses/Unit.js
@@ -796,13 +796,9 @@ var Unit = IgeEntityPhysics.extend({
 						// if old unit type had a same attribute, then take the value from it.
 						if (oldAttributes && oldAttributes[attrId]) {
 							attributeValue = oldAttributes[attrId].value;
-							// attributeMax = oldAttributes[attrId].max;
-							// attributeMin = oldAttributes[attrId].min;
 						}
 
 						if (this._stats.attributes[attrId]) {
-							// this._stats.attributes[attrId].max = attributeMax;
-							// this._stats.attributes[attrId].min = attributeMin;
 							this._stats.attributes[attrId].value = Math.max(data.attributes[attrId].min, Math.min(data.attributes[attrId].max, parseFloat(attributeValue)));
 						}
 					}
