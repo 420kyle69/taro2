f899022862c71d1848a0cfa09208ec3e35df71d2
ipranshujain <re.pranshu@gmail.com>
2023-03-06 17:20:51 +0530

    Added Description to Shop Modal

--- src/gameClasses/ClientNetworkEvents.js
+++ src/gameClasses/ClientNetworkEvents.js
@@ -128,7 +128,11 @@ var ClientNetworkEvents = {
 	_onOpenShop: function (data) {
 		if (data.type) {
 			var shopName = taro.game.data.shops[data.type] ? taro.game.data.shops[data.type].name : 'Item shop';
+			var shopDescription = taro.game.data.shops[data.type] ? taro.game.data.shops[data.type].description : '';
 			$('#modd-item-shop-header').text(shopName);
+			
+			shopDescription?.length > 0 && $('#modd-item-shop-description').text(shopDescription);
+
 			taro.shop.openItemShop(data.type);
 			$('#modd-item-shop-modal').modal('show');
 			var player = taro.client.myPlayer;
