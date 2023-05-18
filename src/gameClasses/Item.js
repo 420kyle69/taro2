8659924481ea64fb1d8cc527d88c54e60ea4659e
sloont <colinabel93@gmail.com>
2023-05-18 11:03:54 -0400

    no logic changes; formatting only

--- src/gameClasses/Item.js
+++ src/gameClasses/Item.js
@@ -817,7 +817,7 @@ var Item = TaroEntityPhysics.extend({
 						rotate += unitAnchorOffsetRotate;
 					}
 
-					
+
 					var unitAnchoredPosition = {
 						x: (unitAnchorOffsetX * Math.cos(unitRotate)) + (unitAnchorOffsetY * Math.sin(unitRotate)),
 						y: (unitAnchorOffsetX * Math.sin(unitRotate)) - (unitAnchorOffsetY * Math.cos(unitRotate))
@@ -859,7 +859,7 @@ var Item = TaroEntityPhysics.extend({
 		self.previousState = null;
 
 		var data = taro.game.getAsset('itemTypes', type);
-		delete data.type // hotfix for dealing with corrupted game json that has unitData.type = "unitType". This is caused by bug in the game editor.
+		delete data.type; // hotfix for dealing with corrupted game json that has unitData.type = "unitType". This is caused by bug in the game editor.
 
 		if (data == undefined) {
 			taro.script.errorLog('changeItemType: invalid data');
@@ -1003,9 +1003,9 @@ var Item = TaroEntityPhysics.extend({
 
 	streamUpdateData: function (queuedData) {
 		var self = this;
-		
+
 		// this was preventing isBeingUsed from streaming hence preventing other players' projectiles from showing
-		// if (taro.isServer && taro.network.isPaused) 
+		// if (taro.isServer && taro.network.isPaused)
 		// 	return;
 
 		TaroEntity.prototype.streamUpdateData.call(this, queuedData);
@@ -1020,6 +1020,7 @@ var Item = TaroEntityPhysics.extend({
 						//self._stats[attrName] = newValue;
 						//this.changeItemType(newValue, {}, false);
 						break;
+
 					case 'ownerUnitId':
 						this._stats[attrName] = newValue;
 						if (taro.isClient) {
@@ -1027,6 +1028,7 @@ var Item = TaroEntityPhysics.extend({
 							self.setOwnerUnit(newOwner);
 						}
 						break;
+
 					case 'scale':
 					case 'scaleBody':
 						this._stats[attrName] = newValue;
@@ -1048,7 +1050,7 @@ var Item = TaroEntityPhysics.extend({
 							self._scaleBox2dBody(newValue);
 						}
 						break;
-					
+
 					case 'hidden':
 						this._stats[attrName] = newValue;
 						if (taro.isClient) {
@@ -1071,14 +1073,15 @@ var Item = TaroEntityPhysics.extend({
 							taro.itemUi.updateItemQuantity(self);
 						}
 						break;
+
 					case 'description':
 						this._stats[attrName] = newValue;
 						var owner = self.getOwnerUnit();
 						if (taro.isClient && taro.client.selectedUnit == owner) {
 							taro.itemUi.updateItemDescription(this);
 						}
-
 						break;
+
 					case 'name':
 						this._stats[attrName] = newValue;
 						var owner = self.getOwnerUnit();
@@ -1095,6 +1098,7 @@ var Item = TaroEntityPhysics.extend({
 							taro.itemUi.updateItemSlot(this, this._stats.slotIndex);
 						}
 						break;
+
 					case 'inventorySlotColor':
 						this._stats[attrName] = newValue;
 						var owner = self.getOwnerUnit();
@@ -1102,10 +1106,11 @@ var Item = TaroEntityPhysics.extend({
 							owner.inventory.update();
 						}
 						break;
+
 					case 'slotIndex':
 						this._stats[attrName] = newValue;
 						break;
-					
+
 					case 'isBeingUsed':
 						var owner = self.getOwnerUnit();
 						// ignore stream so my item use won't fire two bullets
@@ -1133,14 +1138,14 @@ var Item = TaroEntityPhysics.extend({
 
 		var ownerUnit = this.getOwnerUnit();
 		var rotate = this._rotate.z;
-				
+
 		if (ownerUnit && this._stats.stateId != 'dropped') {
-			
+
 			// angleToTarget is only available in server
 			if (taro.isServer && ownerUnit.angleToTarget) {
-				rotate = ownerUnit.angleToTarget;			
+				rotate = ownerUnit.angleToTarget;
 			}
-			
+
 			if (self._stats.currentBody && self._stats.currentBody.jointType == 'weldJoint') {
 				rotate = ownerUnit._rotate.z;
 			}
@@ -1149,7 +1154,6 @@ var Item = TaroEntityPhysics.extend({
 			var x = ownerUnit._translate.x + self.anchoredOffset.x;
 			var y = ownerUnit._translate.y + self.anchoredOffset.y;
 
-			
 			if (taro.isServer || (taro.isClient && taro.client.selectedUnit == ownerUnit)) {
 				if (
 					self._stats.controls &&
@@ -1170,7 +1174,7 @@ var Item = TaroEntityPhysics.extend({
 
 			// run both server & client.
 			// it's important that this runs on client side, because it prepares this item's position when it's dropped
-			self.translateTo(x, y);			
+			self.translateTo(x, y);
 			self.rotateTo(0, 0, rotate);
 
 			// if (this.getOwnerUnit() != taro.client.selectedUnit)	 {
@@ -1178,7 +1182,7 @@ var Item = TaroEntityPhysics.extend({
 			// }
 
 			if (taro.game.cspEnabled && taro.isClient) {
-				self.finalKeyFrame[1] = [x, y, rotate] // prepare position for when this item's dropped. without this, item will appear at an incorrect position
+				self.finalKeyFrame[1] = [x, y, rotate]; // prepare position for when this item's dropped. without this, item will appear at an incorrect position
 			}
 		}
 
