452ad523e8b7603e42c8574593773a319fced037
VietnamNeko <58660787+VietnamNeko@users.noreply.github.com>
2023-07-12 14:03:57 -0300

    make particles only run on client

--- src/gameClasses/components/script/ActionComponent.js
+++ src/gameClasses/components/script/ActionComponent.js
@@ -1427,20 +1427,24 @@ var ActionComponent = TaroEntity.extend({
 						/* particles */
 
 					case 'emitParticleOnceAtPosition':
-						var position = self._script.variable.getValue(action.position, vars);
-						var particleTypeId = self._script.variable.getValue(action.particleType, vars);
-						var angle = self._script.variable.getValue(action.angle, vars);
-						if (particleTypeId && position) {
-							taro.network.send('particle', { particleId: particleTypeId, position: position, angle: angle || 0});     
+						if (taro.isClient) {
+							var position = self._script.variable.getValue(action.position, vars);
+							var particleTypeId = self._script.variable.getValue(action.particleType, vars);
+							var angle = self._script.variable.getValue(action.angle, vars);
+							if (particleTypeId && position) {
+								taro.client.emit('create-particle', { particleId: particleTypeId, position: position, angle: angle || 0});  
+							}
 						}
 						break;
-
+						
 					case 'emitParticleAttachedToEntity':
-						var particleTypeId = self._script.variable.getValue(action.particleType, vars);
-						var angle = self._script.variable.getValue(action.angle, vars);
-						var entity = self._script.variable.getValue(action.entity, vars);
-						if (particleTypeId && entity) {
-							taro.network.send('particle', { particleId: particleTypeId, position: {x:0, y:0}, angle: angle || 0, entityId: entity.id()});
+						if (taro.isClient) {
+							var particleTypeId = self._script.variable.getValue(action.particleType, vars);
+							var angle = self._script.variable.getValue(action.angle, vars);
+							var entity = self._script.variable.getValue(action.entity, vars);
+							if (particleTypeId && entity) {
+								taro.client.emit('create-particle', { particleId: particleTypeId, position: {x:0, y:0}, angle: angle || 0, entityId: entity.id()});
+							};
 						}
 						break;
 
