84645a958e97afb5579a57c84e20bb1668348101
sloont <colinabel93@gmail.com>
2023-08-11 09:59:03 -0400

    let custom w/h from action dictate projectile size

--- engine/core/TaroEntity.js
+++ engine/core/TaroEntity.js
@@ -4717,7 +4717,7 @@ var TaroEntity = TaroObject.extend({
 					break;
 
 				case 'projectile':
-					keys = ['type', 'anim', 'stateId', 'flip', 'sourceItemId', 'streamMode'];
+					keys = ['type', 'anim', 'stateId', 'flip', 'sourceItemId', 'streamMode', 'width', 'height'];
 					data = {
 						attributes: {},
 						// variables: {}
