8a523e9eb5a98ad3208d9fd4d18cb4ae1192a28c
Pankaj Patidar <pankaj9296@gmail.com>
2023-09-20 21:24:59 +0530

    cache update: proxy all s3 requests via cloudflare - validations added

--- src/renderer/phaser/scenes/PhaserScene.js
+++ src/renderer/phaser/scenes/PhaserScene.js
@@ -20,7 +20,7 @@ var PhaserScene = /** @class */ (function (_super) {
     }
     PhaserScene.prototype.patchAssetUrl = function (url) {
         // proxy all s3 requests via cloudflare, old-cache.modd.io points to same 'modd' s3 bucket
-        if (url.startsWith('https://modd.s3.amazonaws.com')) {
+        if (url === null || url === void 0 ? void 0 : url.startsWith('https://modd.s3.amazonaws.com')) {
             url = url.replace('https://modd.s3.amazonaws.com', 'https://old-cache.modd.io');
         }
         // https://stackoverflow.com/a/37455118
