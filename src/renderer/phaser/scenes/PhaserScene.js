14ed31c192e1bf044b261922010e61b7ebbc96f9
Pankaj Patidar <pankaj9296@gmail.com>
2023-09-20 18:25:10 +0530

    cache update: proxy all s3 requests via cloudflare

--- src/renderer/phaser/scenes/PhaserScene.js
+++ src/renderer/phaser/scenes/PhaserScene.js
@@ -19,6 +19,10 @@ var PhaserScene = /** @class */ (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     PhaserScene.prototype.patchAssetUrl = function (url) {
+        // proxy all s3 requests via cloudflare, old-cache.modd.io points to same 'modd' s3 bucket
+        if (url.startsWith('https://modd.s3.amazonaws.com')) {
+            url = url.replace('https://modd.s3.amazonaws.com', 'https://old-cache.modd.io');
+        }
         // https://stackoverflow.com/a/37455118
         return "".concat(url, "?v=1");
     };
