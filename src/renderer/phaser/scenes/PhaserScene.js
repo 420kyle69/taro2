aebd7bbe9a1d89d6d66bcdd6bba5043ea36ec5f9
Pankaj Patidar <pankaj9296@gmail.com>
2023-10-05 16:24:18 +0530

    optimize image loading by using Image tag loader and CF Image optimization

--- src/renderer/phaser/scenes/PhaserScene.js
+++ src/renderer/phaser/scenes/PhaserScene.js
@@ -23,6 +23,18 @@ var PhaserScene = /** @class */ (function (_super) {
         if (url === null || url === void 0 ? void 0 : url.startsWith('https://modd.s3.amazonaws.com')) {
             url = url.replace('https://modd.s3.amazonaws.com', 'https://old-cache.modd.io');
         }
+        var shouldOptimize = new URL(window.location.href).searchParams.get('optimize');
+        if (shouldOptimize && ((url === null || url === void 0 ? void 0 : url.indexOf('.png')) !== -1 || (url === null || url === void 0 ? void 0 : url.indexOf('.jpg')) !== -1)) {
+            // load images via Image tag, helps CF optimize and convert(to webp) images via Polish - https://newdocs.phaser.io/docs/3.60.0/focus/Phaser.Loader.LoaderPlugin-image
+            this.load.imageLoadType = 'HTMLImageElement';
+            // use cache2 and old-cache2 endpoints for images which serve cached and optimized images 
+            url = url.replace('https://cache.modd.io', 'https://cache2.modd.io');
+            url = url.replace('https://old-cache.modd.io', 'https://old-cache2.modd.io');
+        }
+        else {
+            // default loader type
+            this.load.imageLoadType = 'XHR';
+        }
         // https://stackoverflow.com/a/37455118
         return "".concat(url, "?v=1");
     };
