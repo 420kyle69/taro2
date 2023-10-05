231cd16a3fde59147cffab8ab4f15b7c030d4a7b
Pankaj Patidar <pankaj9296@gmail.com>
2023-10-05 17:50:30 +0530

    load all images via Image tag

--- ts/src/renderer/phaser/scenes/PhaserScene.ts
+++ ts/src/renderer/phaser/scenes/PhaserScene.ts
@@ -7,15 +7,9 @@ class PhaserScene extends Phaser.Scene {
 			url = url.replace('https://modd.s3.amazonaws.com', 'https://old-cache.modd.io');
 		}
 
-		const shouldOptimize = new URL(window.location.href).searchParams.get('optimize');
-		if (shouldOptimize && (url?.indexOf('.png') !== -1 || url?.indexOf('.jpg') !== -1)) {
+		if (url?.indexOf('.png') !== -1 || url?.indexOf('.jpg') !== -1) {
 			// load images via Image tag, helps CF optimize and convert(to webp) images via Polish - https://newdocs.phaser.io/docs/3.60.0/focus/Phaser.Loader.LoaderPlugin-image
 			this.load.imageLoadType = 'HTMLImageElement';
-
-			// use cache2 and old-cache2 endpoints for images which serve cached and optimized images 
-			url = url.replace('https://cache.modd.io', 'https://cache2.modd.io');
-			url = url.replace('https://old-cache.modd.io', 'https://old-cache2.modd.io');
-			
 		} else {
 			// default loader type
 			this.load.imageLoadType = 'XHR';
