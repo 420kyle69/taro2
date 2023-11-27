4c853d39be8a8d69302fd52ca05e2bec9fa6a4e0
sloont <colinabel93@gmail.com>
2023-11-27 11:57:35 -0500

    remove physics tick comment and log

--- src/gameClasses/components/GameComponent.js
+++ src/gameClasses/components/GameComponent.js
@@ -52,9 +52,6 @@ var GameComponent = TaroEntity.extend({
 		taro.timer.startGameClock();
 
 		taro._physicsTickRate = Math.max(20, Math.min(60, self.data.frameRate || 20));
-		// taro._physicsTickRate = 60;
-
-		console.log("taro.physics._physicsTickRate", taro._physicsTickRate);
 
 		taro.clusterClient && taro.clusterClient.gameStarted();
 	},
