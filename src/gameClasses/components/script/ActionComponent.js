732dad9c13267d8945fcfab6cb5561c8416b9a7f
toho the Professional Newbie <wongtoho921@gmail.com>
2023-12-13 20:40:17 +0800

    Update ActionComponent.js

--- src/gameClasses/components/script/ActionComponent.js
+++ src/gameClasses/components/script/ActionComponent.js
@@ -1117,28 +1117,7 @@ var ActionComponent = TaroEntity.extend({
 							}
 						}
 						break;
-					
-					case 'doWhile':
-						var loopCounter = 0;
 
-						do {
-							var brk = self.run(action.actions, vars, actionPath);
-							if (brk == 'break' || vars.break) {
-								vars.break = false;
-								break;
-							} else if (brk == 'return') {
-								throw new Error('return without executing script');
-							}
-
-							loopCounter++;
-							if (loopCounter > 10000) {
-								taro.server.unpublishQueued = true;
-								throw new Error("infinite loop detected");
-							}
-						}
-						while (self._script.condition.run(action.conditions, vars, actionPath))
-						break;
-					
 					case 'for':
 						var variables = taro.game.data.variables;
 						var variableName = action.variableName;
@@ -3074,17 +3053,6 @@ var ActionComponent = TaroEntity.extend({
 							object[key] = value;
 						}
 
-						break;
-
-					case 'addNumberElement':
-						var key = self._script.variable.getValue(action.key, vars);
-						var value = self._script.variable.getValue(action.value, vars);
-						var object = self._script.variable.getValue(action.object, vars);
-
-						if (object && key && value) {
-							object[key] = parseFloat(value);
-						}
-						
 						break;
 
 					case 'addObjectElement':
