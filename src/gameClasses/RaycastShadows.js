ca8ef3fd83ba1ae18391a4107b7fbfa023392a82
sloont <colinabel93@gmail.com>
2024-01-04 20:32:11 -0500

    more testing

--- src/gameClasses/RaycastShadows.js
+++ src/gameClasses/RaycastShadows.js
@@ -8,15 +8,40 @@ var RaycastShadows = /** @class */ (function () {
         var graph = this.scene.add.graphics();
         graph.setDepth(41000);
         graph.lineStyle(3, 0x000, 1);
+        var vertices = [];
+        var hEdges = [];
+        var vEdges = [];
         taro.$$('wall').forEach(function (wall) {
-            var box = {
-                x: wall._translate.x - wall._bounds2d.x2,
-                y: wall._translate.y - wall._bounds2d.y2,
-                x2: wall._bounds2d.x,
-                y2: wall._bounds2d.y,
-            };
-            graph.strokeRect(box.x, box.y, box.x2, box.y2);
+            var x = wall._translate.x - wall._bounds2d.x2;
+            var y = wall._translate.y - wall._bounds2d.y2;
+            var w = wall._bounds2d.x;
+            var h = wall._bounds2d.y;
+            vertices.push([x, y], [x + w, y], [x + w, y + h], [x, y + h]);
+            hEdges.push([[x, x + w], y], [[x, x + w], y + h]);
+            vEdges.push([[y, y + h], x], [[y, y + h], x + w]);
         });
+        graph.lineStyle(2, 0x00FF66, 1);
+        hEdges.forEach(function (e) {
+            graph.lineBetween(e[0][0], e[1], e[0][1], e[1]);
+        });
+        graph.lineStyle(2, 0xFFFF99, 1);
+        vEdges.forEach(function (e) {
+            graph.lineBetween(e[1], e[0][0], e[1], e[0][1]);
+        });
+        vertices = _.uniqWith(vertices, _.isEqual);
+        graph.fillStyle(0xFF9999, 0.8);
+        vertices.forEach(function (v) {
+            graph.fillCircle(v[0], v[1], 4);
+        });
+        // arbitrary static player position
+        var play = { x: 300, y: 300 };
+        // draw lines from player to each vertex
+        graph.lineStyle(2, 0x0099FF, 1);
+        var v;
+        for (var _i = 0, vertices_1 = vertices; _i < vertices_1.length; _i++) {
+            v = vertices_1[_i];
+            graph.lineBetween(play.x, play.y, v[0], v[1]);
+        }
     };
     return RaycastShadows;
 }());
--- ts/src/gameClasses/RaycastShadows.ts
+++ ts/src/gameClasses/RaycastShadows.ts
@@ -1,3 +1,5 @@
+type vertex = [number, number];
+type edge = [vertex, number];
 class RaycastShadows {
 	polygons = [];
 	scene: GameScene;
@@ -12,16 +14,58 @@ class RaycastShadows {
 		graph.setDepth(41000);
 		graph.lineStyle(3, 0x000, 1);
 
-		taro.$$('wall').forEach((wall) => {
-			const box = {
-				x: wall._translate.x - wall._bounds2d.x2,
-				y: wall._translate.y - wall._bounds2d.y2,
-				x2: wall._bounds2d.x,
-				y2: wall._bounds2d.y,
-			};
+		let vertices: vertex[] = [];
+		const hEdges: edge[] = [];
+		const vEdges: edge[] = [];
 
-			graph.strokeRect(box.x, box.y, box.x2, box.y2);
+		taro.$$('wall').forEach((wall) => {
+
+			const x = wall._translate.x - wall._bounds2d.x2;
+			const y = wall._translate.y - wall._bounds2d.y2;
+			const w = wall._bounds2d.x;
+			const h = wall._bounds2d.y;
+			vertices.push(
+				[x, y],
+				[x + w, y],
+				[x + w, y + h],
+				[x, y + h]
+			);
+			hEdges.push(
+				[[x, x + w], y],
+				[[x, x + w], y + h]
+			);
+			vEdges.push(
+				[[y, y + h], x],
+				[[y, y + h], x + w]
+			);
 		});
+
+		graph.lineStyle(2, 0x00FF66, 1);
+		hEdges.forEach((e) => {
+			graph.lineBetween(e[0][0], e[1], e[0][1], e[1]);
+		});
+		graph.lineStyle(2, 0xFFFF99, 1);
+		vEdges.forEach((e) => {
+			graph.lineBetween(e[1], e[0][0], e[1], e[0][1]);
+		});
+
+		vertices = _.uniqWith(vertices, _.isEqual);
+
+		graph.fillStyle(0xFF9999, 0.8);
+		vertices.forEach((v) => {
+			graph.fillCircle(v[0], v[1], 4);
+		});
+
+		// arbitrary static player position
+		const play = { x: 300, y: 300 };
+
+		// draw lines from player to each vertex
+		graph.lineStyle(2, 0x0099FF, 1);
+
+		let v: vertex;
+		for (v of vertices) {
+			graph.lineBetween(play.x, play.y, v[0], v[1]);
+		}
 	}
 }
 
