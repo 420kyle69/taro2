ad294fa32b590339a919531964111da2afcfdbf8
Nick van Urk <nickyvanurk@gmx.com>
2024-01-17 17:10:33 +0100

    Render the floor and walls of map

--- src/renderer/three/ThreeRenderer.js
+++ src/renderer/three/ThreeRenderer.js
@@ -7,15 +7,45 @@ var ThreeRenderer = /** @class */ (function () {
         (_a = document.querySelector('#game-div')) === null || _a === void 0 ? void 0 : _a.appendChild(renderer.domElement);
         this.renderer = renderer;
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.camera.position.z = 5;
+        this.camera.position.y = 20;
+        this.camera.position.z = 20;
+        this.camera.lookAt(new THREE.Vector3());
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         this.controls.enableDamping = true;
         this.scene = new THREE.Scene();
         var geometry = new THREE.BoxGeometry(1, 1, 1);
         var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         var cube = new THREE.Mesh(geometry, material);
-        this.scene.add(cube);
-        this.cube = cube;
+        var map = new THREE.Group();
+        map.translateX(-taro.game.data.map.width / 2);
+        map.translateZ(-taro.game.data.map.height / 2);
+        this.scene.add(map);
+        taro.game.data.map.layers.forEach(function (layer) {
+            if (layer.name === 'floor') {
+                for (var x = 0; x < layer.width; x++) {
+                    for (var z = 0; z < layer.height; z++) {
+                        var newCube = cube.clone();
+                        newCube.position.set(x, 0, z);
+                        newCube.material = newCube.material.clone();
+                        newCube.material.color.setScalar(layer.data[z * layer.width + x] / 300);
+                        map.add(newCube);
+                    }
+                }
+            }
+            if (layer.name === 'walls') {
+                for (var x = 0; x < layer.width; x++) {
+                    for (var z = 0; z < layer.height; z++) {
+                        if (layer.data[z * layer.width + x] !== 0) {
+                            var newCube = cube.clone();
+                            newCube.position.set(x, 1, z);
+                            newCube.material = newCube.material.clone();
+                            newCube.material.color.setScalar(layer.data[z * layer.width + x] / 300);
+                            map.add(newCube);
+                        }
+                    }
+                }
+            }
+        });
         this.setupInputListeners();
         requestAnimationFrame(this.render.bind(this));
         taro.client.rendererLoaded.resolve();
@@ -38,8 +68,6 @@ var ThreeRenderer = /** @class */ (function () {
     };
     ThreeRenderer.prototype.render = function () {
         requestAnimationFrame(this.render.bind(this));
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
         this.controls.update();
         this.renderer.render(this.scene, this.camera);
         taro.client.emit('tick');
--- ts/src/renderer/three/ThreeRenderer.ts
+++ ts/src/renderer/three/ThreeRenderer.ts
@@ -5,7 +5,6 @@ class ThreeRenderer {
 	private camera: THREE.PerspectiveCamera;
 	private controls: OrbitControls;
 	private scene: THREE.Scene;
-	private cube: THREE.Mesh;
 
 	constructor() {
 		const renderer = new THREE.WebGLRenderer();
@@ -14,7 +13,9 @@ class ThreeRenderer {
 		this.renderer = renderer;
 
 		this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-		this.camera.position.z = 5;
+		this.camera.position.y = 20;
+		this.camera.position.z = 20;
+		this.camera.lookAt(new THREE.Vector3());
 
 		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 		this.controls.enableDamping = true;
@@ -24,8 +25,39 @@ class ThreeRenderer {
 		const geometry = new THREE.BoxGeometry(1, 1, 1);
 		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 		const cube = new THREE.Mesh(geometry, material);
-		this.scene.add(cube);
-		this.cube = cube;
+
+		const map = new THREE.Group();
+		map.translateX(-taro.game.data.map.width / 2);
+		map.translateZ(-taro.game.data.map.height / 2);
+		this.scene.add(map);
+
+		taro.game.data.map.layers.forEach((layer) => {
+			if (layer.name === 'floor') {
+				for (let x = 0; x < layer.width; x++) {
+					for (let z = 0; z < layer.height; z++) {
+						const newCube = cube.clone();
+						newCube.position.set(x, 0, z);
+						newCube.material = newCube.material.clone();
+						newCube.material.color.setScalar(layer.data[z * layer.width + x] / 300);
+						map.add(newCube);
+					}
+				}
+			}
+
+			if (layer.name === 'walls') {
+				for (let x = 0; x < layer.width; x++) {
+					for (let z = 0; z < layer.height; z++) {
+						if (layer.data[z * layer.width + x] !== 0) {
+							const newCube = cube.clone();
+							newCube.position.set(x, 1, z);
+							newCube.material = newCube.material.clone();
+							newCube.material.color.setScalar(layer.data[z * layer.width + x] / 300);
+							map.add(newCube);
+						}
+					}
+				}
+			}
+		});
 
 		this.setupInputListeners();
 
@@ -56,9 +88,6 @@ class ThreeRenderer {
 
 	render() {
 		requestAnimationFrame(this.render.bind(this));
-
-		this.cube.rotation.x += 0.01;
-		this.cube.rotation.y += 0.01;
 
 		this.controls.update();
 		this.renderer.render(this.scene, this.camera);
