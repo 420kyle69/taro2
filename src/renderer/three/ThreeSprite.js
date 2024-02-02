c2148babc296d8daf07920f594146869dd3919c0
Nick van Urk <nickyvanurk@gmx.com>
2024-02-02 14:27:36 +0100

    Replace entity sprites with a plane

--- src/renderer/three/ThreeSprite.js
+++ src/renderer/three/ThreeSprite.js
@@ -1,16 +1,23 @@
 class ThreeSprite extends THREE.Group {
     constructor(tex) {
         super();
-        const spriteMaterial = new THREE.SpriteMaterial({ transparent: true, map: tex });
-        const sprite = new THREE.Sprite(spriteMaterial);
-        this.sprite = sprite;
-        this.add(sprite);
+        const geometry = new THREE.PlaneGeometry(1, 1);
+        geometry.rotateX(-Math.PI / 2);
+        const material = new THREE.MeshBasicMaterial({ map: tex, transparent: true });
+        this.plane = new THREE.Mesh(geometry, material);
+        this.add(this.plane);
+        // const spriteMaterial = new THREE.SpriteMaterial({ transparent: true, map: tex });
+        // const sprite = new THREE.Sprite(spriteMaterial);
+        // this.sprite = sprite;
+        // this.add(sprite);
     }
     setScale(sx, sy, sz = 1) {
-        this.sprite.scale.set(sx, sy, sz);
+        // this.sprite.scale.set(sx, sy, sz);
+        this.plane.scale.set(sx, sy, sz);
     }
     setRotationY(rad) {
-        this.sprite.material.rotation = rad;
+        // this.sprite.material.rotation = rad;
+        this.plane.rotation.y = rad;
     }
 }
 //# sourceMappingURL=ThreeSprite.js.map
--- ts/src/renderer/three/ThreeSprite.ts
+++ ts/src/renderer/three/ThreeSprite.ts
@@ -1,20 +1,29 @@
 class ThreeSprite extends THREE.Group {
-	private sprite: THREE.Sprite;
+	// private sprite: THREE.Sprite;
+	private plane: THREE.Mesh;
 
 	constructor(tex: THREE.Texture) {
 		super();
 
-		const spriteMaterial = new THREE.SpriteMaterial({ transparent: true, map: tex });
-		const sprite = new THREE.Sprite(spriteMaterial);
-		this.sprite = sprite;
-		this.add(sprite);
+		const geometry = new THREE.PlaneGeometry(1, 1);
+		geometry.rotateX(-Math.PI / 2);
+		const material = new THREE.MeshBasicMaterial({ map: tex, transparent: true });
+		this.plane = new THREE.Mesh(geometry, material);
+		this.add(this.plane);
+
+		// const spriteMaterial = new THREE.SpriteMaterial({ transparent: true, map: tex });
+		// const sprite = new THREE.Sprite(spriteMaterial);
+		// this.sprite = sprite;
+		// this.add(sprite);
 	}
 
 	setScale(sx: number, sy: number, sz = 1) {
-		this.sprite.scale.set(sx, sy, sz);
+		// this.sprite.scale.set(sx, sy, sz);
+		this.plane.scale.set(sx, sy, sz);
 	}
 
 	setRotationY(rad: number) {
-		this.sprite.material.rotation = rad;
+		// this.sprite.material.rotation = rad;
+		this.plane.rotation.y = rad;
 	}
 }
