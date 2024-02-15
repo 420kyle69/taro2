37507cf2b140884f5333c5fb4f2d921bf0a001f8
Nick van Urk <nickyvanurk@gmx.com>
2024-02-15 14:24:55 +0100

    Move image resize func to utils

--- ts/src/renderer/three/ThreeTileset.ts
+++ ts/src/renderer/three/ThreeTileset.ts
@@ -13,26 +13,9 @@ class ThreeTileset {
 		this.originalWidth = texture.image.width;
 		this.originalHeight = texture.image.height;
 		this.columns = Math.floor(texture.image.width / tileWidth);
-		texture.image = resizeImageToPowerOf2(texture.image);
+		texture.image = Utils.resizeImageToPowerOf2(texture.image);
 		this.width = texture.image.width;
 		this.height = texture.image.height;
 		texture.generateMipmaps = false;
 	}
-}
-
-function resizeImageToPowerOf2(image) {
-	const ceil = THREE.MathUtils.ceilPowerOfTwo;
-
-	const width = ceil(image.width);
-	const height = ceil(image.height);
-
-	const canvas = document.createElement('canvas');
-
-	canvas.width = width;
-	canvas.height = height;
-
-	const context = canvas.getContext('2d');
-	context.drawImage(image, 0, 0, width, height, 0, 0, width, height);
-
-	return canvas;
 }
