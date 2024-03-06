d864c1e1479b1f2a6ba0b048bd7ccbeba46b9346
Nick van Urk <nickyvanurk@gmx.com>
2024-03-06 14:04:35 +0100

    Remove group node from floating text; no need for it

--- ts/src/renderer/three/ThreeFloatingText.ts
+++ ts/src/renderer/three/ThreeFloatingText.ts
@@ -1,20 +1,17 @@
 class ThreeFloatingText {
-	node = new THREE.Object3D();
-	label: ThreeLabel;
+	node: ThreeLabel;
 
 	constructor(x: number, y: number, z: number, text: string, color: string) {
-		this.node.position.set(x, y, z);
-
 		// TODO(nick): Dispose label texture properly, should I add destroy flag
 		// to nodes and destroy them all in one go?
-		this.label = new ThreeLabel(text, color, true);
-		this.node.add(this.label);
+		this.node = new ThreeLabel(text, color, true);
+		this.node.position.set(x, y, z);
 
 		new TWEEN.Tween({ offsetInPixels: 0, opacity: 1 })
 			.to({ offsetInPixels: 40, opacity: 0.5 }, 2500)
 			.onUpdate(({ offsetInPixels, opacity }) => {
-				this.label.setOffset(new THREE.Vector2(0, offsetInPixels));
-				this.label.setOpacity(opacity);
+				this.node.setOffset(new THREE.Vector2(0, offsetInPixels));
+				this.node.setOpacity(opacity);
 			})
 			.onComplete(() => this.node.removeFromParent())
 			.start();
