474508541ad38d05a7823d2554f11a0acbbf64e0
Nick van Urk <nickyvanurk@gmx.com>
2024-04-01 19:56:02 +0200

    Add vscode settings

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 var Game = TaroClass.extend({
-	classId: 'Game',
-	init(App, options) {
-		// Create the engine
-		taro = new TaroEngine(options);
-		console.log('taro initialized', taro.isClient, taro.isServer);
-		if (taro.isClient) {
-			taro.client = new App();
-		}
-		if (taro.isServer) {
-			taro.server = new App(options);
-		}
-	},
+    classId: 'Game',
+    init(App, options) {
+        // Create the engine
+        taro = new TaroEngine(options);
+        console.log('taro initialized', taro.isClient, taro.isServer);
+        if (taro.isClient) {
+            taro.client = new App();
+        }
+        if (taro.isServer) {
+            taro.server = new App(options);
+        }
+    },
 });
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-	module.exports = Game;
-} else {
-	var game = new Game(Client);
+    module.exports = Game;
+}
+else {
+    var game = new Game(Client);
 }
 //# sourceMappingURL=index.js.map
