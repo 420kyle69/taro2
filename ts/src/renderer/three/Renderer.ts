d92d4ab60af88a1f54032e940111cf0ac5858224
MoeThun <dhtmitmi@gmail.com>
2024-04-30 23:26:08 +0800

    feat: add mousedownHandle for add-entities

--- ts/src/renderer/three/Renderer.ts
+++ ts/src/renderer/three/Renderer.ts
@@ -185,6 +185,69 @@ namespace Renderer {
 										this.voxelEditor.handleMapToolEdit();
 										break;
 									}
+									case 'add-entities': {
+										const entityData = this.entityEditor.activeEntity;
+										if (entityData) {
+											const worldPoint = this.raycastFloor(0);
+											const entity =
+												taro.game.data[entityData.entityType] && taro.game.data[entityData.entityType][entityData.id];
+											let actionType: string;
+											let height: number;
+											let width: number;
+											if (entityData.entityType === 'unitTypes') {
+												actionType = 'createEntityForPlayerAtPositionWithDimensions';
+												if (entity.bodies?.default) {
+													height = entity.bodies.default.height;
+													width = entity.bodies.default.width;
+												} else {
+													console.log('no default body for unit', entityData.id);
+													return;
+												}
+											} else if (entityData.entityType === 'itemTypes') {
+												actionType = 'createEntityAtPositionWithDimensions';
+												if (entity.bodies?.dropped) {
+													height = entity.bodies.dropped.height;
+													width = entity.bodies.dropped.width;
+												} else {
+													console.log('no dropped body for item', entityData.id);
+													return;
+												}
+											} else if (entityData.entityType === 'projectileTypes') {
+												actionType = 'createEntityAtPositionWithDimensions';
+												if (entity.bodies?.default) {
+													height = entity.bodies.default.height;
+													width = entity.bodies.default.width;
+												} else {
+													console.log('no default body for projectile', entityData.id);
+													return;
+												}
+											}
+
+											const action: ActionData = {
+												type: actionType,
+												entity: entityData.id,
+												entityType: entityData.entityType,
+												position: {
+													function: 'xyCoordinate',
+													x: Math.floor(worldPoint.x),
+													y: Math.floor(worldPoint.z),
+												},
+												width: width,
+												height: height,
+												angle: 0,
+												actionId: taro.newIdHex(),
+												wasCreated: true,
+											};
+											if (entityData.entityType === 'unitTypes') {
+												action.player = {
+													variableName: entityData.player,
+													function: 'getVariable',
+												};
+											}
+											this.createEntityPreview(action);
+											taro.network.send<any>('editInitEntity', action);
+										}
+									}
 								}
 							} else if (
 								Utils.isRightButton(event.buttons) &&
