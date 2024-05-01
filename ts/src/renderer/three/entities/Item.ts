91db929da3fea289fae81f9713a5b7bda8f30738
Nick van Urk <nickyvanurk@gmx.com>
2024-05-01 14:03:18 +0200

    Refactor item to use body component instead of inheritance

--- ts/src/renderer/three/entities/Item.ts
+++ ts/src/renderer/three/entities/Item.ts
@@ -1,8 +1,10 @@
 namespace Renderer {
 	export namespace Three {
-		export class Item extends AnimatedSprite {
+		export class Item extends Node {
 			ownerUnitId: string | undefined;
 			ownerUnit: Unit | undefined;
+
+			body: AnimatedSprite;
 
 			constructor(
 				public taroId: string,
@@ -10,7 +12,10 @@ namespace Renderer {
 				spriteSheet: TextureSheet,
 				public taroEntity?: TaroEntityPhysics
 			) {
-				super(spriteSheet);
+				super();
+
+				this.body = new AnimatedSprite(spriteSheet);
+				this.add(this.body);
 
 				this.ownerUnitId = taroEntity._stats.ownerUnitId;
 			}
@@ -25,11 +30,11 @@ namespace Renderer {
 				taroEntity.on('scale', (data: { x: number; y: number }) => entity.scale.set(data.x, 1, data.y), this);
 				taroEntity.on('show', () => (entity.visible = true), this);
 				taroEntity.on('hide', () => (entity.visible = false), this);
-				taroEntity.on('layer', (layer) => entity.setLayer(layer));
-				taroEntity.on('depth', (depth) => entity.setDepth(depth));
-				taroEntity.on('z-offset', (offset) => entity.setZOffset(Utils.pixelToWorld(offset)));
-				taroEntity.on('flip', (flip) => entity.setFlip(flip % 2 === 1, flip > 1));
-				taroEntity.on('billboard', (isBillboard) => entity.setBillboard(isBillboard, renderer.camera));
+				taroEntity.on('layer', (layer) => entity.body.setLayer(layer));
+				taroEntity.on('depth', (depth) => entity.body.setDepth(depth));
+				taroEntity.on('z-offset', (offset) => entity.body.setZOffset(Utils.pixelToWorld(offset)));
+				taroEntity.on('flip', (flip) => entity.body.setFlip(flip % 2 === 1, flip > 1));
+				taroEntity.on('billboard', (isBillboard) => entity.body.setBillboard(isBillboard, renderer.camera));
 
 				taroEntity.on(
 					'transform',
@@ -51,18 +56,18 @@ namespace Renderer {
 									entity.position.x += x;
 									entity.position.z += y;
 								} else {
-									entity.sprite.position.x = x;
-									entity.sprite.position.z = y;
+									entity.body.sprite.position.x = x;
+									entity.body.sprite.position.z = y;
 								}
 							}
-						} else if (entity.sprite.position.x != 0 || entity.sprite.position.z != 0) {
-							entity.sprite.position.x = 0;
-							entity.sprite.position.z = 0;
+						} else if (entity.body.sprite.position.x != 0 || entity.body.sprite.position.z != 0) {
+							entity.body.sprite.position.x = 0;
+							entity.body.sprite.position.z = 0;
 						}
 
-						entity.setRotationY(-data.rotation);
+						entity.body.setRotationY(-data.rotation);
 						const flip = taroEntity._stats.flip;
-						entity.setFlip(flip % 2 === 1, flip > 1);
+						entity.body.setFlip(flip % 2 === 1, flip > 1);
 					},
 					this
 				);
@@ -79,7 +84,7 @@ namespace Renderer {
 					const key = `${spriteSheet.key}/${id}/${taroEntity._stats.id}`;
 					const animation = AnimationManager.instance().animations.get(key);
 					if (animation) {
-						entity.loop(animation.frames, animation.fps, animation.repeat);
+						entity.body.loop(animation.frames, animation.fps, animation.repeat);
 					}
 				});
 
@@ -90,7 +95,7 @@ namespace Renderer {
 					const sheet = textureMgr.getTextureSheetShallowCopy(key);
 
 					const replaceTexture = (spriteSheet: TextureSheet) => {
-						entity.setTextureSheet(sheet);
+						entity.body.setTextureSheet(sheet);
 						const bounds = taroEntity._bounds2d;
 						entity.setScale(Utils.pixelToWorld(bounds.x), Utils.pixelToWorld(bounds.y));
 					};
@@ -101,14 +106,14 @@ namespace Renderer {
 						const cols = taroEntity._stats.cellSheet.columnCount;
 						const rows = taroEntity._stats.cellSheet.rowCount;
 						textureMgr.loadTextureSheetFromUrl(key, Utils.patchAssetUrl(key), cols, rows, () => {
-							animationMgr.createAnimationsFromTaroData(key, taroEntity._stats);
+							animationMgr.createAnimationsFromTaroData(key, taroEntity._stats as unknown as EntityData);
 							replaceTexture(sheet);
 						});
 					}
 				});
 
 				taroEntity.on('fading-text', (data: { text: string; color?: string }) => {
-					const size = entity.getSizeInPixels();
+					const size = entity.body.getSizeInPixels();
 					const offsetInPixels = -25 - size.height * 0.5;
 					const text = new FloatingText(0, 0, 0, data.text || '', data.color || '#ffffff', 0, -offsetInPixels);
 					entity.add(text);
@@ -129,8 +134,12 @@ namespace Renderer {
 				}
 			}
 
+			update(dt: number) {
+				this.body.update(dt);
+			}
+
 			setScale(sx: number, sy: number) {
-				super.setScale(sx, sy);
+				this.body.setScale(sx, sy);
 			}
 		}
 	}
