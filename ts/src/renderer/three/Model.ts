a34a3e4095be679323abfbfcb8efde8b0acdcc87
Nick van Urk <nickyvanurk@gmx.com>
2024-05-02 20:02:57 +0200

    Add aabb functions to model

--- ts/src/renderer/three/Model.ts
+++ ts/src/renderer/three/Model.ts
@@ -1,13 +1,27 @@
 namespace Renderer {
 	export namespace Three {
 		export class Model extends Node {
+			private scene: THREE.Group;
+			private aabb = new THREE.Box3();
+			private size = new THREE.Vector3();
+			private center = new THREE.Vector3();
+
 			constructor(name: string) {
 				super();
 
 				const model = gAssetManager.getModel(name);
-				if (model) {
-					this.add(model.scene.clone());
-				}
+				this.scene = model.scene.clone();
+				this.add(this.scene);
+			}
+
+			getSize() {
+				this.aabb.setFromObject(this.scene);
+				return this.aabb.getSize(this.size);
+			}
+
+			getCenter() {
+				this.aabb.setFromObject(this.scene);
+				return this.aabb.getCenter(this.center);
 			}
 		}
 	}
