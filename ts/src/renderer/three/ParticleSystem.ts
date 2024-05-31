08baf461a8823fb93050322f7bb6be11dcc6f9b8
Nick van Urk <nickyvanurk@gmx.com>
2024-05-31 19:33:44 +0200

    Render particles with right render order

--- ts/src/renderer/three/ParticleSystem.ts
+++ ts/src/renderer/three/ParticleSystem.ts
@@ -86,6 +86,7 @@ namespace Renderer {
 					points.matrixAutoUpdate = false;
 					// eslint-disable-next-line @typescript-eslint/no-empty-function
 					points.updateMatrixWorld = function () {};
+					points.renderOrder = 498; // Higher than entities, lower than HUD
 
 					this.add(points);
 				}
